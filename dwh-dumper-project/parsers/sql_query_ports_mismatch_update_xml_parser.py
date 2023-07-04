62b2f71d14fa9ac1551e65b217bd126b2f3f7b31
maciejsicinski <maciej.sicinski@gmail.com>
2023-07-04 11:21:12 +0200

    Adding functions to get connected ports, adding function to get sql query value, cleanup, moving table attribute handling to transformation block

--- dwh-dumper-project/parsers/sql_query_ports_mismatch_update_xml_parser.py
+++ dwh-dumper-project/parsers/sql_query_ports_mismatch_update_xml_parser.py
@@ -13,6 +13,26 @@ output_dir_path = "/Users/MaciejSicinski/xml_parsing/dwh-dumper-project/sql_extr
 # Create the output directory if it doesn't exist
 os.makedirs(output_dir_path, exist_ok=True)
 
+def getSqlQuery(transformation):
+    sql_query =[]
+    for ta in transformation.iter("TABLEATTRIBUTE"):
+        sql_query.append(getValue(ta, ["Sql Query"]))
+    return sql_query[0]
+
+def getConnectedFrom(mapping, transformation_name):
+    connected_from = []
+    for connector in mapping.iter("CONNECTOR"):
+        if connector.attrib["FROMINSTANCE"] == transformation_name:
+            connected_from.append(connector.attrib["FROMFIELD"])
+    return connected_from
+
+def getConnectedTo(mapping, transformation_name):
+    connected_from = []
+    for connector in mapping.iter("CONNECTOR"):
+        if connector.attrib["TOINSTANCE"] == transformation_name:
+            connected_from.append(connector.attrib["TOFIELD"])
+    return connected_from
+
 def find_columns(query):
     column_names = []
     try:
@@ -54,44 +74,55 @@ def process_file(filename):
         doc = ET.parse(fh)
         root = doc.getroot()
         for folder in root.iter("FOLDER"):
+            folder_name = folder.attrib["NAME"]
             for child in folder.iter("MAPPING"):
+                mapping_name = child.attrib["NAME"]
                 for sq in child.iter("TRANSFORMATION"):
-                    tname = sq.attrib["NAME"]
                     ttype = sq.attrib["TYPE"]
+                    transformation_name = sq.attrib["NAME"]
+                    connected_from = getConnectedFrom(child, transformation_name)
+                    connected_to = getConnectedTo(child, transformation_name)
+                    # For each transformation get only Sql Query attribute to parse and save columns returned by a SQL query   
+                    val = getSqlQuery(sq)
                     # For each transformation in each mapping get only Source Qualifier transformation and save it's port
-                    if ttype == "Source Qualifier":                      
+                    if ttype == "Source Qualifier" and val is not None and len(val) > 0:                      
                         port_names = []
                         field_nodes = list(sq.iter("TRANSFORMFIELD"))
+                        print(folder_name)
+                        print(mapping_name)
+                        print(transformation_name)
                         for field in field_nodes:
-                            # HERE ADD TO EXTRACT ONLY CONNECTED PORTS
-                            port_names.append(field.attrib["NAME"])
-                        port_names = [item.lower() for item in port_names]
-                    # For each transformation get only Sql Query attribute to parse and save columns returned by a SQL query    
-                    for ta in sq.iter("TABLEATTRIBUTE"):
-                        val = getValue(ta, ["Sql Query"])
-                        if val is not None and len(val) > 0:
-                            sql_columns = []
-                            sql_columns = find_columns(val)
+                                print (field.attrib["NAME"])
+                        #Only manipulate ports if they are connected (port is used further down the mapping)
+                                if field.attrib["NAME"] in connected_from:
+                                    port_names.append(field.attrib["NAME"])
+                        print (sorted(port_names))
+                        print (sorted(connected_from))
+                        print (sorted(connected_to))
+                        #port_names = [item.lower() for item in port_names]
+                        sql_columns = []
+                        sql_columns = find_columns(val)
+                        print(sorted(sql_columns))
                             # HERE ADD EMPTY ALIASES HANDLING
-                            sql_columns= [item.lower() for item in sql_columns]
+                            #sql_columns= [item.lower() for item in sql_columns]
                             # Don't update if the SQL parser returned an error
-                            if sql_columns == ["parse_error"]:
-                                continue
-                            else:
+                        if sql_columns == ["parse_error"]:
+                            continue
+                        else:
                             # Don't update if both lists contains the same values TO ADD: it only holds true if all ports are connected!!    
-                                if port_names == sql_columns:
-                                    continue
+                            if port_names == sql_columns:
+                                continue
                             # Update NAME attribute values
-                                for i, field in field_nodes:   
-                                    field = field_nodes[i]                             
-                                    if i < len(sql_columns):
+                            for i, field in enumerate(sq.iter("TRANSFORMFIELD")):                           
+                                if i < len(sql_columns):
+                                    if sql_columns[i]:
                                         field.attrib["NAME"] = sql_columns[i] 
                                 # HERE ADD NAME CONFLICT RESOLUTION
                             # Update XML file with modified values
 
                                 # HERE ADD XML FORMATTING/ENCODING/ETC to fit the source file format
-                                with open(filename, "w", encoding="iso-8859-15") as fh:
-                                    fh.write(ET.tostring(root).decode("iso-8859-15"))
+                                #with open(filename, "w", encoding="iso-8859-15") as fh:
+                                 #   fh.write(ET.tostring(root).decode("iso-8859-15"))
                                     
                                     #tree = ET.ElementTree(root)
                                     #tree.write(filename, encoding="iso-8859-15", xml_declaration=True)
