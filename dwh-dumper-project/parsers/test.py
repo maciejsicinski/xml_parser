0c214cca339283a4bf62d1d99b598a1159c64c8b
maciejsicinski <maciej.sicinski@gmail.com>
2023-07-10 13:56:04 +0200

    Removing sqlglot part (no support for updating empty aliases)

---
+++ dwh-dumper-project/parsers/test.py
@@ -0,0 +1,34 @@
+import sqlglot
+from sqlglot import exp
+
+def addColumnAlias(sql_query):
+    try:
+        parsed = sqlglot.parse_one(sql_query)
+
+        # Find all expressions in the SELECT statement
+        expressions = parsed.find(exp.Select).args["expressions"]
+        # Find the expression corresponding to the column name
+        for i, expression in enumerate(expressions):
+            if expression and not expression.alias and hasattr(expression, '__dict__'):
+                new_expression = str(expression) + " " + "AS " + f"expression_{i+1}" 
+                print(new_expression)
+                parsed_expression = sqlglot.parse_one(new_expression)
+                print(parsed_expression)
+                parsed_expression = parsed_expression.find(exp.Expression)
+                print(parsed_expression)
+        # Get the modified SQL query
+        modified_query = str(parsed)
+
+        return modified_query
+
+    except Exception as e:
+        print(f"Error occurred while parsing the SQL query: {e}")
+
+
+# Example SQL query
+sql_query = "SELECT col1 aa, col2 + col3, col4 FROM table1 WHERE col5 > 10"
+
+# Add alias for column "col2"
+modified_query = addColumnAlias(sql_query)
+
+print(modified_query)
--- dwh-dumper-project/parsers/updating_empty_aliases.py
+++ dwh-dumper-project/parsers/updating_empty_aliases.py
@@ -1,6 +1,4 @@
 import re
-import sqlglot
-from sqlglot import exp
 
 def assignAliases(sql_query):
     # Pattern for matching expressions in the SELECT statement
@@ -12,54 +10,28 @@ def assignAliases(sql_query):
         expressions = select_match.group(0).strip().split(',')
 
         # Assign aliases to expressions without existing aliases
-        modified_query = re.sub(pattern, ', '.join(f"{expr} AS expr_{i+1}" if not re.search(r"\s+AS\s+", expr, re.I) else expr for i, expr in enumerate(expressions)), sql_query)
+        modified_expressions = []
+        for i, expr in enumerate(expressions):
+            expr = expr.strip()
+            if not re.search(r"\s+AS\s+", expr, re.I):
+                modified_expressions.append(f"{expr} AS expr_{i+1}")
+            else:
+                modified_expressions.append(expr)
+
+        # Update the SQL query with modified expressions and add spaces
+        modified_query = re.sub(pattern, ', '.join(modified_expressions), sql_query)
+        modified_query = modified_query.replace("SELECT", "SELECT ").replace("FROM", " FROM")
 
         return modified_query
 
     else:
         print("No SELECT statement found in the SQL query.")
         return sql_query
-    
-def assignAliasesSqlglot(sql_query):
-    try:
-        parsed = sqlglot.parse_one(sql_query)
-
-        # Find all expressions in the SELECT statement
-        expressions = parsed.find(exp.Select).args["expressions"]
-
-        # Assign aliases to expressions without existing aliases
-        for i, expression in enumerate(expressions):
-            if not expression.alias:
-                print("empty_expression") 
-               # print(expression.alias)
-                #expression.alias = f"expr_{i+1}" # CANT DO THAT
-
-        # Get the modified SQL query
-        modified_query = str(parsed)
-
-        return modified_query
-
-    except Exception as e:
-        print(f"Error occurred while parsing the SQL query: {e}")
-
-def find_columns(query):
-    column_names = []
-    try:
-        for expression in sqlglot.parse_one(query).find(exp.Select).args["expressions"]:
-            column_names.append(expression.alias_or_name)
-    except Exception as e:
-        return ["parse_error"]
-    return column_names
 
 # Example SQL query
 sql_query = "SELECT col1, col2 + col3 FROM table1 WHERE col4 > 10"
 
 # Assign aliases to expressions without existing aliases
 modified_query = assignAliases(sql_query)
-#modified_query_sqlglot = assignAliasesSqlglot(sql_query)
 
 print(modified_query)
-
-#print(modified_query_sqlglot)
-
-
